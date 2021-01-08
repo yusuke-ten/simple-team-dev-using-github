ca4730e76a3e2cd50c12c77579288d392ec83cbc
yusuke-ten <net.escape.com@gmail.com>
2021-01-09 03:08:57 +0900

    cutom-buttonコンポーネントをtypescriptで書き直した

--- src/components/custom-button/custom-button.component.tsx
+++ src/components/custom-button/custom-button.component.tsx
@@ -1,12 +1,19 @@
-import React from 'react';
+import React, { ReactNode } from 'react';
 import './custom-button.styles.scss';
+
+interface Props extends React.HTMLProps<HTMLButtonElement> {
+  children: ReactNode;
+  isGoogleSignIn: boolean;
+  inverted: boolean;
+  type?: 'button' | 'submit' | 'reset';
+}
 
 const CustomButton = ({
   children,
   isGoogleSignIn,
   inverted,
   ...otherProps
-}) => (
+}: Props): ReactNode => (
   <button
     className={`${inverted ? 'inverted' : ''}${
       isGoogleSignIn ? 'google-sign-in' : ''
